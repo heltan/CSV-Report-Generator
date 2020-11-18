354865430731ca44c7d2b03f2251b2a71a147854
heltan <helen.tanubrata@sfcm.edu>
2020-11-17 23:42:39 -0800

    able to post data from the form input, into the localhost server page

--- server.js
+++ server.js
@@ -3,10 +3,20 @@
 //import express from 'express';
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('client'));
-app.listen(port);
-app.post('http://localhost:3000/', (req, res) => {
-    console.log('being posted');
+
+
+app.listen(port, () => {
+    console.log('server running on', port);
+});
+//now it posts to the server, but the req is empty?
+app.post('/', (req, res) => {
+    console.log('test');
+    console.log('req', req.body);
+    res.send(req.body);
 });
 
