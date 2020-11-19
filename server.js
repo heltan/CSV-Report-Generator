4cff725ccd4e1ba5a630bf42a18c4c051dfe563f
heltan <helen.tanubrata@sfcm.edu>
2020-11-18 20:55:04 -0800

    It displays the csv onto the page

--- server.js
+++ server.js
@@ -14,21 +14,6 @@ app.listen(port, () => {
     console.log('server running on', port);
 });
 
-//below is my test json object
-/*
-{
-    "employee": {
-        "name":       "bob",
-        "salary":      56000,
-        "married":    true
-    },
-    "employee": {
-        "name":       "alice",
-        "salary":      1,
-        "married":    false
-    }
-}
-*/
 //here when user submits form, it posts to the server
 app.post('/', (req, res) => {
     //req.body.text is the text of the submit input
@@ -38,23 +23,23 @@ app.post('/', (req, res) => {
     let obj = (req.body.text);
     obj = JSON.parse(obj);
 
-    json2csv(obj);
-
-
-
+    let csv = json2csv(obj);
 
     //send it back in the response body
-    res.send(req.body.text);
+    res.send(csv);
 });
 
 //here is a function to concert json to csv and return the csv
 let json2csv = (obj) => {
 
   //now we want to take out every key and put it into a string, separated with commas
-  let columns = getKeys(obj);
-  let columns = returnKeys;
+  let keys = getKeys(obj);
+  //now we want to get the rows
   getRows(obj);
-
+  let rows = returnRows;
+  //now we put it together into one string
+  let stringForcsv = keys + '\n' + rows;
+  return stringForcsv;
 
 };
 
@@ -65,9 +50,11 @@ let keysArr = [];
 let getKeys = (object) => {
   for (let keys in object) {
    //only add keys to the returnKeys if it doesn't already exist. no duplicate column names
+    let currentRow = '';
     if (!returnKeys.includes(keys.toString())) {
         returnKeys += '"' + keys+ '"' + ',';
         keysArr.push(keys);
+        //now if there is a key and it's not an object, put the value
     }
     if (typeof object[keys] === 'object') {
       getKeys(object[keys]);
@@ -101,4 +88,52 @@ app.get('/blah', (req, res) => {
   console.log('asdfasdlkfja');
   res.send('you tried to reach this page?');
 });
-
+//test object below
+let obj = {
+  "firstName": "Joshie",
+  "lastName": "Wyattson",
+  "county": "San Mateo",
+  "city": "San Mateo",
+  "role": "Broker",
+  "sales": 1000000,
+  "children": [
+  {
+    "firstName": "Beth Jr.",
+    "lastName": "Johnson",
+    "county": "San Mateo",
+    "city": "Pacifica",
+    "role": "Manager",
+    "sales": 2900000,
+    "children": [
+      {
+        "firstName": "Smitty",
+        "lastName": "Won",
+        "county": "San Mateo",
+        "city": "Redwood City",
+        "role": "Sales Person",
+        "sales": 4800000,
+        "children": []
+      },
+      {
+        "firstName": "Allen",
+        "lastName": "Price",
+        "county": "San Mateo",
+        "city": "Burlingame",
+        "role": "Sales Person",
+        "sales": 2500000,
+        "children": []
+      }
+    ]
+  },
+  {
+    "firstName": "Beth",
+    "lastName": "Johnson",
+    "county": "San Francisco",
+    "city": "San Francisco",
+    "role": "Broker/Sales Person",
+    "sales": 7500000,
+    "children": []
+  }
+]
+};
+//json2csv(obj);
